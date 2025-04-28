b30fc140588abfccc74b3433c94502e19e301054
Pedro Delgadillo <91334249+petedillo@users.noreply.github.com>
2025-04-27 23:18:38 -0500

    update API logic to use .env variables

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,16 +2,18 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000',
+    baseURL:
+        import.meta.env.VITE_API_BASE_URL
+        || 'http://localhost:3000',
 });
 
 // Add a request interceptor to attach the JWT token to each request
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 });
 
 export default API;
